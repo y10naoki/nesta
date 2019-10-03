23c08232e783c73efa56fa760687deec2b40e9c1
Naoki YAMAMOTO <y10naoki@gmail.com>
2019-10-03 13:45:20 +0900

    Initial Commit

---
+++ nesta/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  nesta
+//
+//  Created by YAMAMOTO Naoki on 2019/10/03.
+//  Copyright © 2019 YAMAMOTO Naoki. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
